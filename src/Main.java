fe6d79a8da3059e671623c0faaeb912ee2e7eee7
alikeli <eli-com2@yandex.ru>
2022-08-28 17:59:37 +0300

    Выполненое дз2

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,17 @@ public class Main {
             count++;
         }
         System.out.println(" ");
-        for (int i = 10; i > 0; --i) {
+        for (int i = 10; i > 0; i--) {
             System.out.print(i + " ");
         }
         System.out.println(" ");
 
         //Task 2
 
-        for (int dayNumber = 2; dayNumber <= 31; dayNumber++) {
-            if ((dayNumber - 2) % 7 == 0) {
+        for (int dayNumber = 2; dayNumber <= 31; dayNumber+=7) {
+            //if ((dayNumber - 2) % 7 == 0) {
                 System.out.println("Сегодня пятница, " + dayNumber + " число. Необходимо подготовить отчет.");
             }
-        }
 
         //Task 3
 
@@ -29,7 +28,8 @@ public class Main {
         int secondBorder = currentYear + 100;
         int startYear = firstBorder - firstBorder % (firstBorder / 79);
 
-        for (int i = startYear + 79; i < secondBorder; i = i + 79) {
+        for (int i = startYear + 79; i <=  secondBorder; i += 79) {
+            if(i > firstBorder)
 
             System.out.println(i);
 
